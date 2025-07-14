5c130f43bb3e2a4b2dc9cef407d6805adc38a487
cjcj <66452117+chengjiang123@users.noreply.github.com>
2025-07-14 01:15:04 +0100

    add ternary matmul

--- Layers/MatMul.h
+++ Layers/MatMul.h
@@ -44,3 +44,29 @@ matmul_transpose_scale_row_loop:
 		}
 	}
 }
+
+template<typename T, int rows, int hidden, int cols>
+void ternary_matmul(
+    T A[rows][hidden],
+    int8_t B[hidden][cols],  // Ternary weights: -1, 0, or 1
+    T result[rows][cols]
+) {
+ternary_matmul_row:
+    for (int i = 0; i < rows; i++) {
+    ternary_matmul_col:
+        for (int j = 0; j < cols; j++) {
+            T sum = 0;
+        ternary_matmul_k:
+            for (int k = 0; k < hidden; k++) {
+                #pragma HLS PIPELINE II=1
+                if (B[k][j] == 1) {
+                    sum += A[i][k];
+                } else if (B[k][j] == -1) {
+                    sum -= A[i][k];
+                }
+                // For 0: no operation
+            }
+            result[i][j] = sum;
+        }
+    }
+}
